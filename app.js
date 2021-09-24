2cd642c846f0604c627ca6fc51c727e2758c956a
Carolin <carolin.raudam@khk.ee>
2021-09-24 13:00:17 +0300

    Math

--- app.js
+++ app.js
@@ -1,38 +1,31 @@
- // tüübiteisendus
-
-  let val;
-
-//number -> string
-
-  val = String(555);
-  val = String(4+4);
-  val = String(5.6);
-
-// Boolean
-  val = String(true);
-  val = String(false);
-
-// Object -> String
-  val = String(new Date ());
-
-//Array -> String
-  val = String([1, 2, 3, 4, 5])
-
-// toString
-  val = (5).toString()
-
-//String -> Number
-  val = Number('5');
-  val = Number('6.7'); 
-  val = Number(true);
-  val = Number(false); //--> annab 0
-  val = Number(null);
-  val = Number('tere'); //--> ei saa
-  val = [1, 2, 3]       //--> ei saa
+const num1 = 100;
+const num2 = 50;
 
 
+let val;
+
+//Mate tehted
+
+val = num1 - num2;
+val = num1 + num2;
+val = num1 * num2;
+val = num1 / num2;   //--Ei kasutata
+val = num1 ** num2; //--Ei kasutata
+
+val = num1 % num2;
+
+//MATH
+
+val = Math.PI;
+
+val = Math.round(2.8);
+val = Math.ceil(2.4);     //-- Ülesse poole
+val = Math.floor(2.8);   //-- Alla poole
+
+val = Math.sqrt(64);   //--SquareRoot
+val = Math.abs(-3);   //--Absolute
+val = Math.pow(4, 2) //--Astendamine
+val = Math.round(Math.random() * 10);
 
 
-
- console.log(typeof val);
- console.log(val);
+console.log(val)
