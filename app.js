4294e90f3fbfc3b2152de390de2218bfb05a3f27
Carolin <carolin.raudam@khk.ee>
2021-09-24 12:48:47 +0300

    Types of conversion

--- app.js
+++ app.js
@@ -1,41 +1,38 @@
-// Andmetüübid
+ // tüübiteisendus
+
+  let val;
+
+//number -> string
+
+  val = String(555);
+  val = String(4+4);
+  val = String(5.6);
+
+// Boolean
+  val = String(true);
+  val = String(false);
+
+// Object -> String
+  val = String(new Date ());
+
+//Array -> String
+  val = String([1, 2, 3, 4, 5])
+
+// toString
+  val = (5).toString()
+
+//String -> Number
+  val = Number('5');
+  val = Number('6.7'); 
+  val = Number(true);
+  val = Number(false); //--> annab 0
+  val = Number(null);
+  val = Number('tere'); //--> ei saa
+  val = [1, 2, 3]       //--> ei saa
 
 
-// Lihtsad andmed - Primitives
-
- // ---TEKST
-	const name= "Carolin Raudam";
-
-	const age = 17;
-
-	const height = 1.73;
 
 
 
- // --- TÕEVÄÄRTUSD
-	const haskids = false
-
-	const goestoschool = true
-
-
-
- // --- TÜHIVÄÄRTUSED
-	const hasairplane = null;
-
-
-//Keerulised andmed - REFERENCE TYPES
-
-
- // ---MASSIIV 
-
- 	const hobbies = ['music', 'sport'];
-
- // ---OBJECT
- 	const person = {
- 		name: 'Caroin',
- 		age :17
- };
-
-console.log(person);
-console.log(typeof person);
-
+ console.log(typeof val);
+ console.log(val);
