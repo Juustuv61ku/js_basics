4801c275eee332ae8eace715cf2846b62fe9b6fe
Carolin <carolin.raudam@khk.ee>
2021-09-24 11:17:42 +0300

    Variables

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("test");
+//Muutuja
+let name = "Carolin Raudam"
+name = "Carolin Klais";
+let firstname = "Carolin";
+let $ = "Dollar";
+
+
+
+// Muutujua, mis on konstantne 
+const surname = "Raudam";
+// surname = "Klais";
+
+
+
+//Massivid
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+
+
+//Objekt
+const person = {
+	name: "Carolin",
+	age: 17
+};
+
+person.age = 18;
+console.log(numbers);
+console.log(person);
+
