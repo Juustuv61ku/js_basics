3428c88c3a241d9f7d2d6b44592f29eaf1d2690f
Carolin <carolin.raudam@khk.ee>
2021-09-27 14:48:13 +0300

    IF Statment

--- app.js
+++ app.js
@@ -1,17 +1,104 @@
-const today = new Date
+/*
+If(tingimus){
+    //tingimused ifi puhul
+} else {
+    //tegevused else puhul
+}
+*/
 
 
-val = today; //consoolist vali <prototype>, et saada cheatcode B)
-
-birthday = new Date('10/02/2021 12:00_36');
 
 
-birthday.setMonth(2);
-birthday.setDate(12);
-birthday.setFullYear(2020);
-birthday.setHour(17);
-birthday.setMinutes(38);
-birthday.setSeconds(40);
-birthday.setMilliseconds(15);
 
-console.log(birtday)
+//võrdub -- ==
+//võrdub + tüübikontroll-- ===
+//ei võrdu -- !=
+//ei võrdu + tüübikontroll .. !==
+
+
+
+
+
+
+const id = 100
+
+/*if(id == 100){
+    console.log('Ok!');
+} else {
+    console.log('NOT Ok!');
+}
+*/
+
+
+
+
+
+
+
+/*let testId;
+
+ if(testId != 'undefined'){
+console.log('Id is ok!');
+} else {
+    console.log('Id is not Ok!')
+}
+*/ 
+
+
+
+
+/*
+if(testId > '100'){
+console.log('Id is greater than 100!');
+} else {
+    console.log('Id is equal or less than 100!')
+}
+
+*/
+
+
+
+
+
+/*
+ let color = 'yellow';
+
+ if(color === 'red'){
+    console.log('STOP!')
+ } if(color === 'yellow'){
+    console.log('ATTENTION!')
+ } if(color === 'green'){
+    console.log('GO!')
+ } else {
+    console.log('This color is not allowed!')
+ }
+*/
+
+
+
+
+
+ //&& - and
+ //|| -- or
+
+
+/*
+ let name = 'Kadi';
+ let age = 18;
+
+ if (age > 0 && age <= 12){
+    console.log(`${name} on laps.`);
+ } else if(age > 12 && age < 18){
+    console.log(`${name} on nooruk.`);
+ } else {
+    console.log(`${name} on täiskasvanud.`)
+ }
+
+*/
+
+
+
+
+ 
+console.log(id === 100? 'OK': 'NOT OK')
+ 
