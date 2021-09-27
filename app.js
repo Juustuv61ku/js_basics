8d505ddac29907484d1b2297768c0369bd125930
Carolin <carolin.raudam@khk.ee>
2021-09-27 13:44:16 +0300

    objects

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-const  numbers = [43,56,33, 23, 44, 36, 5];
-const  numbers2 = new Array(22, 45, 33, 76, 54);
+const person ={
+    firstname : 'Kadi',
+    lastname: 'Tamm',
+    age: 23,
+    email:'Kadi.Tamm@gmail.com',
+    hobbies: ['music', 'sport'],
+    address: {
+        city: 'Tallinn',
+        country: 'Harjumaa'
+     },
+    getBirthyear:function () {
+        return 2021- this.age;
+    } 
 
-const fruits = ['apple', 'banana', 'orange'];
-
-const mixed = [22, 'tere', true, null, {a:1, b:2}, new Date()];
-
-
-
+};
 let val;
 
-//val = numbers.length;                        //massiivi pikkus
-//val = Array.isArray(numbers);               //massiivi kontroll
-//val = numbers[3];                          //kindel väärtus
-//numbers[3] = 100;                         //asendamine
-//numbers.unshift(350);                    //lisamine algusesse
-//numbers.pop();                          //kustutatakse lõpust
-//numbers.shift();                       //kustutatakse algusest
-//val = numbers.indexOf(100);           //mitmes on?
-//numbers.splice(1, 2);                //lõikamine (mitmes arv, kui palju)
-//val = numbers.concat(numbers2);     //ühendamine
-//val = numbers.sort(function(x, y){ //sorteerimine
-//    return x - y;
-//});  
-val = numbers.splice(3, false, 100)         
-console.log(val);
+val = person.firstname;
+val = person['lastname'];
+val = person.age;
+val = person.hobbies[1]
+val = person.address.city;
+val = person.getBirthyear();
+
+console.log(val)
