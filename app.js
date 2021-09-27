502528374c6e343363157ece3d20c8f9de052fab
Carolin <carolin.raudam@khk.ee>
2021-09-27 14:53:51 +0300

    Switch

--- app.js
+++ app.js
@@ -1,104 +1,19 @@
-/*
-If(tingimus){
-    //tingimused ifi puhul
-} else {
-    //tegevused else puhul
-}
-*/
+let color = 'red'
 
-
-
-
-
-//võrdub -- ==
-//võrdub + tüübikontroll-- ===
-//ei võrdu -- !=
-//ei võrdu + tüübikontroll .. !==
-
-
-
-
-
-
-const id = 100
-
-/*if(id == 100){
-    console.log('Ok!');
-} else {
-    console.log('NOT Ok!');
-}
-*/
-
-
-
-
-
-
-
-/*let testId;
-
- if(testId != 'undefined'){
-console.log('Id is ok!');
-} else {
-    console.log('Id is not Ok!')
-}
-*/ 
-
-
-
-
-/*
-if(testId > '100'){
-console.log('Id is greater than 100!');
-} else {
-    console.log('Id is equal or less than 100!')
-}
-
-*/
-
-
-
-
-
-/*
- let color = 'yellow';
-
- if(color === 'red'){
+switch(color){
+    case 'red':
     console.log('STOP!')
- } if(color === 'yellow'){
+    break;
+
+    case 'yellow':
     console.log('ATTENTION!')
- } if(color === 'green'){
+    break;
+
+    case 'green':
     console.log('GO!')
- } else {
+    break;
+    
+    default:
     console.log('This color is not allowed!')
- }
-*/
-
-
-
-
-
- //&& - and
- //|| -- or
-
-
-/*
- let name = 'Kadi';
- let age = 18;
-
- if (age > 0 && age <= 12){
-    console.log(`${name} on laps.`);
- } else if(age > 12 && age < 18){
-    console.log(`${name} on nooruk.`);
- } else {
-    console.log(`${name} on täiskasvanud.`)
- }
-
-*/
-
-
-
-
- 
-console.log(id === 100? 'OK': 'NOT OK')
- 
+    break;
+}
