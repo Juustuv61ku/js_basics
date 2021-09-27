8a07cb6b950749e207d48cad9ddac34e392be344
Carolin <carolin.raudam@khk.ee>
2021-09-27 15:29:09 +0300

    While/ For Loops

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
-let date = new Date();
-
-let daynr = date.getDay();
-
-
-switch (daynr){
-    case 1:
-    console.log('Täna on esmaspäev.')
-    break;
-
-    case 2:
-    console.log('Täna on teisipäev.')
-    break;
- 
-    case 3:
-    console.log('Täna on kolmapäev.')
-    break;
-
-    case 4:
-    console.log('Täna on neljapäev.')
-    break;
-
-    case 5:
-     console.log('Täna on reede.') 
-      break;
-
-    case 6:
-    console.log('Täna on laupäev.')
-    break;
-
-    case 7:
-    console.log('Täna on pühapäev.')
-    break;
+// üldtsükkel
+/*
+for(let i = 0; i < 10; i++){
+    if (i % 2 === 0)
+    console.log(i)
 }
+*/
+
+
+/*
+for(let i = 0; i < 10; i++){
+    //if(i === 5) break;
+    //if (i === 5) continue;
+    console.log(i)
+}
+*/
+
+
+/*
+let i = 0
+while (i < 10){
+    console.log(i)
+    i++;
+}
+*/
+
+
+let j = 0;
+do{
+    console.log(j)
+    i++
+}while(j<10)
