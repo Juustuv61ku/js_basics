e72dc9c4cefe4f2ade19feb69bdcb6fb85ab9521
Carolin <carolin.raudam@khk.ee>
2021-10-01 13:50:57 +0300

    DOM treversy

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 let val;
 
-val = document.getElementById('task-title')
-val = document.getElementById('task-title').style.background='black';
-      document.getElementById('task-title').style.color='white';
-      document.getElementById('task-title').style.padding='5px';
-      document.getElementById('task-title').textContent='My Tasks';
+const list = document.querySelector('url');
+const listItem = document.querySelector('li:first-child');
+
+val = list.children
+val = list.children[1];
+list.children[1].textContent = 'Study XML'
+val = list.children[2].children;
+list.children[0].style.color = 'blue'
+
+
+val = list.firstElementChild
+val = list.lastElementChild
+val = list.childElementCount
+
+
+val = listItem.parentElement
+val = listItem.nextElementSibling
+val = listItem.previousElementsibling
 
 
 
 
-console.log()
+
+
+console.log(val)
