56a2a3fc77c92a5d66b59a6413efa86bf288f9d0
Carolin <carolin.raudam@khk.ee>
2021-10-04 13:50:07 +0300

    Add task

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const list = document.querySelector('ul');
+const form = document.querySelector('form');
 const taskInput = document.querySelector('#task');
 
 
-list.addEventListener('click', runEvent )
+
+form.addEventListener('submit', addTask );
 
 
 
 
- function runEvent(e) {
-       console.log(`Event is ${e.type}`)
-       //something to do
-      console.log(e.target.value)
+ function addTask(e){
+      console.log(taskInput.value);
+      const li = document.createElement('li');
+      const list = document.querySelector('ul');
+      const link = document.createElement('a');
+
+      li.className = 'collection-item';
+      li.appendChild(document.createTextNode(taskInput.value)); 
+
+      link.className = 'secondary-content';
+      link.appendChild(document.createTextNode('X'));
+      link.setAttribute('href', '#');
+
+      li.appendChild(link);
+      list.appendChild(li);
+
+      taskInput.value = '';
+      e.preventDefault();
+ 
  }
 
